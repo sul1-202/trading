96b49c467062b7817896419df12aba68610db546
sul1-202 <sulaim9495@gmail.com>
2025-05-08 09:14:10 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 // src/App.tsx
 import React from "react";
-import Nav from "/home/intern-01/Desktop/aaa/AAA/src/assets/components/Nav.tsx";
+
 import TrustSection from "./components/TrustSection";
 import Hero from "./components/Hero";
 import PricingSection from "./components/PricingSection";
 import EducationSection from "./components/EducationSection";
 import Footer from "./components/Footer";
+import Navbar from "./components/Nav";
 
 
 
 const App: React.FC = () => {
   return (
     <div>
-      <Nav />
+      <Navbar />
       <Hero/>
 
       <TrustSection />
